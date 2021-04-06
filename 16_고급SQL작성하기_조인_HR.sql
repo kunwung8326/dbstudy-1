2253d8b8e90f7f1ae0a33068f9e447d5939dbdae
koreaitlecture <min-k-t@hanmail.net>
2021-04-06 17:41:34 +0900

    update 2021-04-06

--- 16_고급SQL작성하기_조인_HR.sql
+++ 16_고급SQL작성하기_조인_HR.sql
@@ -1,13 +1,58 @@
 -- 1. 부서의 위치(location_id)가 1700 인 사원들의 employee_id, last_name, department_id, salary 를 조회한다.
 -- 사용할 테이블 (departments, employees)
+SELECT
+       e.employee_id
+     , e.last_name
+     , e.department_id
+     , e.salary
+  FROM employees e INNER JOIN departments d  -- employees 테이블이 driving 테이블, departments 테이블이 driven 테이블
+    ON e.department_id = d.department_id  -- 조인 조건 (인덱스가 없는 e.department_id를 기준으로 검색하므로 좋지 않다.)
+ WHERE d.location_id = 1700;  -- 일반 조건
+
+SELECT
+       e.employee_id
+     , e.last_name
+     , e.department_id
+     , e.salary
+  FROM employees e, departments d
+ WHERE e.department_id = d.department_id  -- 조인 조건
+   AND d.location_id = 1700;  -- 일반 조건
 
 
 -- 2. 부서명(department_name)이 'Executive' 인 부서에 근무하는 모든 사원들의 department_id, last_name, job_id 를 조회한다.
 -- 사용할 테이블 (departments, employees)
+SELECT
+       d.department_id
+     , e.last_name
+     , e.job_id
+  FROM departments d INNER JOIN employees e  -- departments 테이블이 driving 테이블, employees 테이블이 driven 테이블
+    ON d.department_id = e.department_id  -- 조건절의 등호(=) 왼쪽은 PK 또는 인덱스를 가진 칼럼이 좋다.
+ WHERE d.department_name = 'Executive';
+
+SELECT
+       d.department_id
+     , e.last_name
+     , e.job_id
+  FROM departments d, employees e
+ WHERE d.department_id = e.department_id
+   AND d.department_name = 'Executive';
 
 
 -- 3. 기존의 직업(job_id)을 여전히 가지고 있는 사원들의 employee_id, job_id 를 조회한다.
 -- 사용할 테이블 (employees, job_history)
+SELECT
+       e.employee_id
+     , e.job_id
+  FROM employees e INNER JOIN job_history j
+    ON e.employee_id = j.employee_id  -- 조인 조건
+ WHERE e.job_id = j.job_id;  -- 일반 조건 : 기존의 직업을 여전히 가지고 있다. employees 테이블의 job_id(현재 job)와 job_history 테이블의 job_id(과거 job)가 같다.
+
+SELECT
+       e.employee_id
+     , e.job_id
+  FROM employees e, job_history j
+ WHERE e.employee_id = j.employee_id
+   AND e.job_id = j.job_id;
 
 
 -- 4. 각 부서별 사원수와 평균연봉을 department_name, location_id 와 함께 조회한다.
