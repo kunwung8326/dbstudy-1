3fad3b57ddf9b1f8964323a130eb10de27a18a3e
koreaitlecture <min-k-t@hanmail.net>
2021-04-07 17:04:35 +0900

    update 2021-04-07

--- 17_고급SQL작성하기_서브쿼리_HR.sql
+++ 17_고급SQL작성하기_서브쿼리_HR.sql
@@ -1,5 +1,5 @@
 -- 1. 모든 사원의 last_name, salary, 본인이 근무하는 부서의 평균연봉(salary)을 조회하시오.
--- 스칼라 서브쿼리
+-- 1) 스칼라 서브쿼리
 -- t : 전체 테이블의 데이터
 -- d : 같은 부서의 데이터
 SELECT t.last_name
@@ -9,6 +9,7 @@ SELECT t.last_name
          WHERE d.department_id = t.department_id) AS 평균연봉
   FROM employees t;
 
+-- 2) 외부조인 (department_id가 없는 사원도 조회할 수 있도록)
 SELECT t.last_name
      , t.salary
      , ROUND(AVG(d.salary)) AS 평균연봉
@@ -18,13 +19,21 @@ SELECT t.last_name
 
 
 -- 2. 부서(department_id)별로 department_id, department_name, 평균연봉(salary)을 조회하시오.
--- 스칼라 서브쿼리
+-- 1) 스칼라 서브쿼리
 SELECT d.department_id
      , d.department_name
      , (SELECT ROUND(AVG(e.salary))
           FROM employees e
-         WHERE e.department_id = d.department_id) AS 평균연봉
+         WHERE d.department_id = e.department_id) AS 평균연봉
   FROM departments d;
+
+-- 2) 외부조인
+SELECT d.department_id
+     , d.department_name
+     , ROUND(AVG(e.salary)) AS 평균연봉
+  FROM departments d LEFT OUTER JOIN employees e
+    ON d.department_id = e.department_id
+ GROUP BY d.department_id, d.department_name;
 
 
 -- 3. 모든 사원들의 employee_id, last_name, department_name 을 조회하시오.
@@ -36,11 +45,11 @@ SELECT e.employee_id
          WHERE d.department_id = e.department_id) AS 부서명
   FROM employees e;
 
--- 2) 내부 조인
+-- 2) 외부조인
 SELECT e.employee_id
      , e.last_name
      , d.department_name
-  FROM departments d INNER JOIN employees e
+  FROM departments d RIGHT OUTER JOIN employees e
     ON d.department_id = e.department_id;
 
 
@@ -52,7 +61,7 @@ SELECT last_name
                     FROM employees);
 
 
--- 5. Patrick Sully 와 같은 부서에 근무하는 모든 사원정보를 조회하시오.
+-- 5. Patrick Sully(first_name: Patrick, last_name: Sully)와 같은 부서(department_id)에 근무하는 모든 사원정보를 조회하시오.
 -- first_name : Patrick
 -- last_name : Sully
 -- 서브쿼리 : Patrick Sully의 부서, 다중 행 서브쿼리
@@ -66,7 +75,7 @@ SELECT first_name
                            AND last_name = 'Sully');
 
 
--- 6. 부서번호가 20인 사원들 중에서 평균연봉 이상의 연봉을 받는 사원정보를 조회하시오.
+-- 6. 부서번호(department_id)가 20인 사원들 중에서 평균연봉(salary) 이상의 연봉을 받는 사원정보를 조회하시오.
 SELECT last_name
      , salary
      , department_id
@@ -76,7 +85,7 @@ SELECT last_name
                     FROM employees);
 
 
--- 7. 직업(job_id)이 'PU_MAN'인 사원들의 최대연봉보다 더 많은 연봉을 받은 사원들의 정보를 조회하시오.
+-- 7. 직업(job_id)이 'PU_MAN'인 사원들의 최대연봉(salary)보다 더 많은 연봉을 받은 사원들의 정보를 조회하시오.
 -- 1) 다중 행 서브쿼리
 SELECT last_name
      , salary
@@ -95,7 +104,8 @@ SELECT last_name
                    FROM employees
                   WHERE job_id = 'PU_MAN');
 
--- 8. 사원번호가 131인 사원의 job_id와 salary가 모두 일치하는 사원들의 정보를 조회하시오.
+
+-- 8. 사원번호(employee_id)가 131인 사원의 job_id와 salary가 모두 일치하는 사원들의 정보를 조회하시오.
 SELECT last_name
      , job_id
      , salary
@@ -116,7 +126,7 @@ SELECT last_name
                             WHERE employee_id = 131);
 
 
--- 9. LOCATION_ID가 1000~1900인 국가들의 COUNTRY_ID와 COUNTRY_NAME을 조회하시오.
+-- 9. location_id가 1000~1900인 국가들의 country_id와 country_name을 조회하시오.
 SELECT country_id
      , country_name
   FROM countries
@@ -125,7 +135,7 @@ SELECT country_id
                       WHERE location_id BETWEEN 1000 AND 1900);
 
 
--- 10. 부서가 'Executive'인 모든 사원들의 정보를 조회하시오.
+-- 10. 부서(department_name)가 'Executive'인 모든 사원들의 정보를 조회하시오.
 -- 서브쿼리 : 부서이름이 'Executive'인 부서들의 department_id
 -- 서브쿼리의 WHERE 절에서 사용한 DEPARTMENT_NAME은 PK, UQ가 아니므로 서브쿼리의 결과는 여러 개이다.
 SELECT last_name
@@ -136,7 +146,7 @@ SELECT last_name
                          WHERE department_name = 'Executive');
 
 
--- 11. 부서번호가 30인 사원들 중에서 부서번호가 50인 사원들의 최대연봉보다 더 많은 연봉을 받는 사원들을 조회하시오.
+-- 11. 부서번호(department_id)가 30인 사원들 중에서 부서번호가 50인 사원들의 최대연봉(salary)보다 더 많은 연봉을 받는 사원들을 조회하시오.
 -- 1) 다중 행 서브쿼리
 SELECT last_name
      , salary
@@ -157,13 +167,60 @@ SELECT last_name
 
 
 -- 12. MANAGER가 아닌 사원들의 정보를 조회하시오.
--- MANAGER는 MANAGER_ID를 가지고 있다.
+-- MANAGER는 manager_id를 가지고 있다.
+-- 서브쿼리의 결과는 NULL 값을 가질 수 없다.
+
+SELECT employee_id
+     , last_name
+  FROM employees
+ WHERE employee_id NOT IN(SELECT DISTINCT manager_id
+                            FROM employees
+                           WHERE manager_id IS NOT NULL);
 
 
--- 13. 근무지가 'Southlake'인 사원들의 정보를 조회하시오.
+-- 13. 근무지(city)가 'Southlake'인 사원들의 정보를 조회하시오.
+-- 서브쿼리1 : 근무지(city)가 'Southlake'인 location_id를 locations 테이블에서 조회
+-- 서브쿼리2 : 모든 사원들의 location_id (employees와 departments의 조인)
+/*
+employees      -  departments                 -  locations
+department_id  -  department_id, location_id  -  location_id, city
+*/
+-- 1) 서브쿼리
+SELECT employee_id
+     , last_name
+  FROM employees e
+ WHERE (SELECT location_id
+          FROM departments d
+         WHERE d.department_id = e.department_id) IN(SELECT location_id
+                                                       FROM locations
+                                                      WHERE city = 'Southlake');
+
+-- 2) 내부조인
+SELECT employee_id
+     , last_name
+  FROM locations l, departments d, employees e
+ WHERE l.location_id = d.location_id
+   AND d.department_id = e.department_id
+   AND l.city = 'Southlake';
 
 
--- 14. 부서명의 가나다순으로 모든 사원의 정보를 조회하시오.
+-- 14. 부서명(department_name)의 가나다순(오름차순)으로 모든 사원의 정보를 조회하시오.
+SELECT employee_id
+     , last_name
+  FROM employees e
+ ORDER BY (SELECT d.department_name
+             FROM departments d
+            WHERE d.department_id = e.department_id); 
 
 
--- 15. 가장 많은 사원들이 근무하고 있는 부서의 번호와 근무하는 인원수를 조회하시오.
+-- 15. 가장 많은 사원들이 근무하고 있는 부서번호(department_id)와 근무하는 인원수를 조회하시오.
+-- 근무 중인 부서의 사원수
+-- 최대 인원이 근무하는 사원수
+SELECT department_id
+     , COUNT(*) AS 사원수
+  FROM employees
+ WHERE department_id IS NOT NULL
+ GROUP BY department_id
+HAVING COUNT(*) = (SELECT MAX(COUNT(*))
+                     FROM employees
+                    GROUP BY department_id);
